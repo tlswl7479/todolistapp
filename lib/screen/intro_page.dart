459183b3c5c4d6a9b8dee0560eeeeffb3567c62d
vvalvvizal <vvalvvizal@gmail.com>
2024-01-12 21:58:55 +0900

    config

---
+++ lib/screen/intro_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+/// 시작 화면 (Splash_Screen)
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 4), () {
+      Navigator.pushNamed(context, '/main');
+    },);
+
+    return const Scaffold(
+      backgroundColor: Colors.limeAccent,
+      // appBar: AppBar(
+      //   title: Text('메인화면'),
+      // ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'TODO',
+            style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 30),
+          ),
+          Text(
+            'LIST',
+            style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 30),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.library_add_check_outlined, size: 100,)
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
